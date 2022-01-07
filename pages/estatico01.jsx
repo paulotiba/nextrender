ce6653b91e7d7c963f4c0e852f1af796ebb97a4c
paulotiba <tibapaulo@outlook.com>
2022-01-07 11:23:51 -0300

    firstlenson

---
+++ pages/estatico01.jsx
@@ -0,0 +1,18 @@
+import {useEffect,  useState } from "react"
+
+export  default function Estatico01() {
+
+    const [ num, setNum ] = useState(0)
+    useEffect(() => {
+        setNum(Math.random())
+     },[])
+
+ 
+
+    return(
+        <div>
+            <h1>Olá Mundo!!! Estático 1 </h1>
+            <h2>{num}</h2>
+        </div>
+    )
+}
